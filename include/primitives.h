c3296b5b5cca9c183a45bde86716373fe0c8c1cf
Mikhail Petrochenkov <mishok2503@mail.ru>
2021-04-24 13:50:15 +0300

    delete primitive

--- include/primitives.h
+++
@@ -1,9 +0,0 @@
-#ifndef __PRIMITIVES_H__
-#define __PRIMITIVES_H__
-
-#include "tgaimage.h"
-#include "vector.h"
-
-void line(TGAImage &image, Vector2i p1, Vector2i p2, const TGAColor &color);
-
-#endif // __PRIMITIVES_H__
